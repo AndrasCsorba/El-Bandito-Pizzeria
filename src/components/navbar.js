0007dc629a40d35eb3edf4888cc5a72b39fbfbc2
AndrasCsorba <andreascs@mail.ch>
2025-01-20 11:05:11 +0100

    navbar and footer

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function navbar() {
+  return (
+    <div className="navbar">
+      <div className="leftSide"></div>
+      <div className="rightSide"></div>
+    </div>
+  );
+}
+
+export default navbar;
