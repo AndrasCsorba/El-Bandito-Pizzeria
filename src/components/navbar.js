03f88ac91b23dcd615cc027629d0b812c93b9e58
AndrasCsorba <andreascs@mail.ch>
2025-01-29 19:08:38 +0100

    Navbar  and app css

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Logo from "../assets/pizzaLogo.png";
 import { Link } from "react-router-dom";
-import "../styles/navbar.css";
+import "../styles/Navbar.css";
 
 function Navbar() {
   return (
