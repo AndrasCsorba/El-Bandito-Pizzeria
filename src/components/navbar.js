2a546b60146b041b07e6b5c59c516086901aaf6d
AndrasCsorba <andreascs@mail.ch>
2025-01-29 18:04:09 +0100

    renamed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Logo from "../assets/pizzaLogo.png";
 
 function navbar() {
   return (
     <div className="navbar">
       <div className="leftSide"></div>
-
+      <img src={Logo} />
       <div className="rightSide"></div>
     </div>
   );
