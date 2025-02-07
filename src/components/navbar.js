e997b1b369c67e85fc9ff30484c8470a5b4f0eaf
AndrasCsorba <andreascs@mail.ch>
2025-02-07 19:25:26 +0100

    bug

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
         <img src={Logo} alt="Logo" />
-        <div classNamer="hiddenLinks">
+        <div className="hiddenLinks">
           <Link to="/">Home</Link>
           <Link to="/Menu">Menu</Link>
           <Link to="/About">About</Link>
@@ -26,7 +26,7 @@ function Navbar() {
         <Link to="/Menu">Menu</Link>
         <Link to="/About">About</Link>
         <Link to="/Contact">Contact</Link>
-        <button omClick={toggleNavbar}>
+        <button onClick={toggleNavbar}>
           <ReorderIcon />
         </button>
       </div>
