62abae8c096d8a064b631368959bf552778ca90b
AndrasCsorba <andreascs@mail.ch>
2025-02-08 16:47:00 +0100

    props json listing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footers from "./components/Footers";
 import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Menu } from "@mui/material";
 
 function App() {
   return (
@@ -10,7 +14,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route path="/" exact component={Home} />
+          <Route path="/menu" exact component={Menu} />
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
         </Routes>
         <Footers />
       </Router>
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { MenuList } from "../helpers/MenuList";
+import MenuItem from "../components/MenuItem";
 
 function Menu() {
   return (
     <div className="menu">
       <h1 className="menuTitel">Our Menu</h1>
-      <div className="menuList"></div>
+      <div className="menuList">
+        {MenuList.map((menuItem, key) => {
+          return <MenuItem />;
+        })}
+      </div>
     </div>
   );
 }
