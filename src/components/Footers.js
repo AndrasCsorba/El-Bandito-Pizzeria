1977d0f07606ea4b90a9b7bf6b8278ec1b68013b
AndrasCsorba <andreascs@mail.ch>
2025-02-08 15:37:09 +0100

    footers css

--- src/components/Footers.js
+++ src/components/Footers.js
@@ -3,6 +3,7 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import XIcon from "@mui/icons-material/X";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "../styles/Footers.css";
 
 function Footers() {
   return (
